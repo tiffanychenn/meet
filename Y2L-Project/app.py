0722774a0a74d530244b3ba01dc393f806662ab8
Tiffany Chen <tchen8@stuy.edu>
2020-01-07 23:44:43 +0200

    hopefully this works

--- Y2L-Project/app.py
+++ Y2L-Project/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, redirect, url_for, render_template
-from flask import session as login_session
+from flask import Flask, request, redirect, url_for, render_template, session
+from databases import *
 
 app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
@@ -13,11 +13,35 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    if "user" in session:
+        return redirect(url_for('root'))
+    if request.method == "GET":
+        return render_template('login.html', loggedin=False)
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        user = query_by_username(username)
+        if password != user[1]:
+            return redirect(url_for('login'))
+        else:
+            session['user'] = user
+            return redirect(url_for('root'))
 
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route('/user_creation', methods=['POST'])
+def user_creation():
+    user = request.form['username']
+    pw = request.form['password']
+    pw_confirm = request.form['confirm']
+    name = request.form['name']
+    if not pw == pw_confirm:
+        return redirect(url_for('register'))
+    else:
+        db.register(user,pw,name)
+        return redirect(url_for('login'))
 
 #####################
 
